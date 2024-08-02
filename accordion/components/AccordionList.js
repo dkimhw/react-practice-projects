1d3f6bd2897468c90859e598f5ebac0fbfb8c070
David Kim <dkhw90@gmail.com>
2024-08-02 18:09:25 +0200

    Practice react by building an accordion component

---
+++ accordion/components/AccordionList.js
@@ -0,0 +1,10 @@
+import { Accordion } from "./Accordion"
+import classes from './AccordionList.module.css'
+
+export const AccordionList = () => {
+  return (
+    <div className={classes['accordion-list']}>
+
+    </div>
+  )
+}
